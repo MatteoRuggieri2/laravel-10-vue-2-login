e6bdb3034e5b6055976971204c869f4448556e60
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2024-03-14 00:27:17 +0100

    Added frontoffice vue routes

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('home');
 
 Route::middleware('auth')
     ->namespace('Admin')
@@ -29,6 +23,6 @@ Route::middleware('auth')
         Route::get('/', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('home');
     });
 
-// Route::get('{any?}', function() {
-//     return view('guests.home');
-// })->where('any', '.*');
+Route::get('{any?}', function() {
+    return view('guests.home');
+})->where('any', '.*');
