ba08f15cf03a637ee77ecaa52847910fbfe6928f
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2024-03-13 22:34:23 +0100

    Fixed dashboard navbar

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -18,22 +18,28 @@
 
     <!-- Styles -->
     {{--! <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+    
+    {{-- Assets --}}
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    
 </head>
 <body>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark flex-md-nowrap p-0">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{ route('admin.home') }}">Boolpress</a>
-        <ul class="navbar-nav px-3 ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('logout') }}"
-                   onclick="event.preventDefault();
-                                 document.getElementById('logout-form').submit();">
-                    Logout
-                </a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
-            </li>
-        </ul>
+        <div class="container-fluid">
+            <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{ route('admin.home') }}">Boolpress</a>
+            <ul class="navbar-nav px-3 ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
+            </ul>
+        </div>
     </nav>
     <div class="container-fluid">
         <div class="row">
