f8553416364dc7033c1e450837ffe06239cb67c2
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2024-03-14 00:28:23 +0100

    Created vue test component - TestComponent.vue

---
+++ resources/js/components/TestComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2>Ciao, sono un componente di test! (TestComponent.vue)</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TestComponent',
+}
+</script>
