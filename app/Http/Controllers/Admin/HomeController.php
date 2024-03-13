2ff215d8306edd1d4e874644cf66ad79c34fe5a1
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2024-03-13 19:50:16 +0100

    Return view home in HomeController (index function)

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -23,7 +23,6 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index() {
-        dd('Dashboard Admin');
-        // return view('home');
+        return view('home');
     }
 }
